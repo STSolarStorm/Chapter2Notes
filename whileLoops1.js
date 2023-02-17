3de291a7d4e4190ac5ded491c2effa8d3800972c
dgmp <dgmp@fresnou.org>
2023-02-17 14:16:45 -0800

    Added whileLoops & whileLoops1

---
+++ whileLoops1.js
@@ -0,0 +1,84 @@
+const readLine = require('readline-sync');
+
+// // Problem Number 1
+// let Prob1 = 0;
+// while (Prob1 <= 25){
+//     console.log(Prob1);
+//     Prob1++;
+// }
+//
+// // Problem Number 2
+// let Prob2 = 0;
+// while (Prob2 <= 50){
+//     console.log(Prob2);
+//     Prob2++;
+// }
+//
+// // Problem Number 3
+// let Prob3 = -10;
+// while (Prob3 <= 10){
+//     console.log(Prob3);
+//     Prob3++;
+// }
+//
+// // Problem Number 4
+// let Prob4 = 0;
+// while (Prob4 >= -50){
+//     console.log(Prob4);
+//     Prob4--;
+// }
+
+// // Problem Number 5
+// let Prob5 = 0;
+// while (Prob5 <= 100){
+//     if(Prob5 % 2 == 0){
+//         console.log(Prob5);
+//     }
+//     Prob5++;
+// }
+
+// // Problem Number 6
+// let Prob6 = 100;
+// while (Prob6 >= 0){
+//     if(Prob6 % 2 == 1){
+//         console.log(Prob6);
+//     }
+//     Prob6--;
+// }
+
+// // Problem Number 7
+// let Prob7 = 0;
+// while (Prob7 <= 100){
+//     if(Prob7 % 3 == 0){
+//         console.log(Prob7);
+//     }
+//     Prob7++;
+// }
+
+// // Problem Number 8
+// let Prob8 = 0;
+// while (Prob8 <= 1000){
+//     if(Prob8 % 5 == 0){
+//         console.log(Prob8);
+//     }
+//     Prob8++;
+// }
+
+// // Problem Number 9
+// let Prob9 = 0;
+// while (Prob9 <= 100){
+//     if(Prob9 < 10 || Prob9 > 25) {
+//         console.log(Prob9);
+//     }
+//     Prob9++;
+// }
+
+// Problem Number 10
+// needs 40 through 50 also
+let Prob10 = 0;
+while (Prob10 <= 100){
+    if(Prob10 < 10 || Prob10 > 25) {
+        console.log(Prob10);
+    }
+    Prob10++;
+}
