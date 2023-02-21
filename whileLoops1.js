108add48b0b0cbc93e67a1b822c18aa869157afc
dgmp <dgmp@fresnou.org>
2023-02-21 14:12:44 -0800

    Finished whileLoops 1 and added whileloops2

--- whileLoops1.js
+++ whileLoops1.js
@@ -6,27 +6,31 @@ const readLine = require('readline-sync');
 //     console.log(Prob1);
 //     Prob1++;
 // }
-//
+
+
 // // Problem Number 2
 // let Prob2 = 0;
 // while (Prob2 <= 50){
 //     console.log(Prob2);
 //     Prob2++;
 // }
-//
+
+
 // // Problem Number 3
 // let Prob3 = -10;
 // while (Prob3 <= 10){
 //     console.log(Prob3);
 //     Prob3++;
 // }
-//
+
+
 // // Problem Number 4
 // let Prob4 = 0;
 // while (Prob4 >= -50){
 //     console.log(Prob4);
 //     Prob4--;
 // }
+
 
 // // Problem Number 5
 // let Prob5 = 0;
@@ -37,6 +41,7 @@ const readLine = require('readline-sync');
 //     Prob5++;
 // }
 
+
 // // Problem Number 6
 // let Prob6 = 100;
 // while (Prob6 >= 0){
@@ -45,6 +50,7 @@ const readLine = require('readline-sync');
 //     }
 //     Prob6--;
 // }
+
 
 // // Problem Number 7
 // let Prob7 = 0;
@@ -55,6 +61,7 @@ const readLine = require('readline-sync');
 //     Prob7++;
 // }
 
+
 // // Problem Number 8
 // let Prob8 = 0;
 // while (Prob8 <= 1000){
@@ -63,6 +70,7 @@ const readLine = require('readline-sync');
 //     }
 //     Prob8++;
 // }
+
 
 // // Problem Number 9
 // let Prob9 = 0;
@@ -73,12 +81,17 @@ const readLine = require('readline-sync');
 //     Prob9++;
 // }
 
-// Problem Number 10
-// needs 40 through 50 also
-let Prob10 = 0;
-while (Prob10 <= 100){
-    if(Prob10 < 10 || Prob10 > 25) {
-        console.log(Prob10);
-    }
-    Prob10++;
-}
+
+// // Problem Number 10
+// // needs 40 through 50 also
+// let Prob10 = 0;
+// while (Prob10 <= 100){
+//     if(Prob10 <= 9) {
+//         console.log(Prob10);
+//     } else if (Prob10 >= 19 && Prob10 <= 39){
+//         console.log(Prob10);
+//     } else if (Prob10 >= 51){
+//         console.log(Prob10);
+//     }
+//     Prob10++;
+// }
---
+++ whileLoops2.js
@@ -0,0 +1,71 @@
+const readLine = require('readline-sync');
+
+// // Problem Number 11 and Number 12
+// let Prob11and12 = readLine.question("Do you wish to quit? Y/N: ");
+// while (Prob11and12 != "Y" && Prob11and12 != "N" && Prob11and12 != "y" && Prob11and12 != "n"){
+//     console.log("Invalid Input");
+//     Prob11and12 = readLine.question("Do you wish to quit? Y/y/N/n: ");
+// }
+
+// // Problem Number 13
+// let Prob13 = readLine.question("Pick either option A, option B, or option C: ");
+// while (Prob13 != "A" && Prob13 != "B" && Prob13 != "C"){
+//     console.log("Invalid Input");
+//     Prob13 = readLine.question("Pick either option A, option B, or option C: ");
+// }
+
+
+// // Problem Number 14
+// let Prob14 = readLine.question("Pick either option 1, option 2, or option 3: ");
+// while (Prob14 != "1" && Prob14 != "2" && Prob14 != "3"){
+//     console.log("Invalid Input");
+//     Prob14 = readLine.question("Pick either option 1, option 2, or option 3: ");
+// }
+
+
+// // Problem Number 15
+// let Prob15 = readLine.question("Pick a number from 0 to 10: ");
+// while (Prob15 > 10){
+//     console.log("Invalid Input");
+//     Prob15 = readLine.question("Pick a number from 0 to 10: ");
+// }
+
+
+// // Problem Number 16
+// let Prob16 = readLine.question("Enter a positive number: ");
+// while (Prob16 < 0){
+//     console.log("Invalid Input");
+//     Prob16 = readLine.question("Enter a positive number: ");
+// }
+
+
+// // Problem Number 17
+// let Prob17 = readLine.question("Enter a negative number: ");
+// while (Prob17 > -1){
+//     console.log("Invalid Input");
+//     Prob17 = readLine.question("Enter a negative number: ");
+// }
+
+
+// // Problem Number 18
+// let Prob18 = readLine.question("Enter an even number: ");
+// while (Prob18 % 2 != 0){
+//     console.log("Invalid Input");
+//     Prob18 = readLine.question("Enter an even number: ");
+// }
+
+
+// Problem Number 19
+let Prob19 = readLine.question("Enter a number that is between 0-10 or between 20-30: ");
+while (Prob19 ){
+    console.log("Invalid Input");
+    Prob19 = readLine.question("Enter a number that is between 0-10 or between 20-30: ");
+}
+
+// if(Prob10 <= 9) {
+//         console.log(Prob10);
+//     } else if (Prob10 >= 19 && Prob10 <= 39){
+//         console.log(Prob10);
+//     } else if (Prob10 >= 51){
+//         console.log(Prob10);
+//     }
